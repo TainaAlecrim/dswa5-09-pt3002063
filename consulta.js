40e90fcf60e1d6c22d836e5e7e792bc660f9b6b0
Taina Alecrim <tai.estudos@gmail.com>
2020-10-04 13:47:34 -0300

    aaa

---
+++ consulta.js
@@ -0,0 +1,31 @@
+const findDocuments = function(db, callback) {
+
+    const collection = db.collection('contatos');
+
+    collection.find({}).toArray(function(err, docs){
+        assert.equal(err, null);
+        console.log("foram encontrados os seguintes docs");
+        console.log(docs)
+        callback(docs);
+    });
+
+};
+
+const MongoCliente = require('mongodb').MongoClient;
+const assert = require('assert');
+
+const url = 'mongodb+srv://dswa5:dswa5@cluster0.vuhwh.mongodb.net/test?retryWrites=true&w=majority';
+
+const dbName = 'ifsp';
+
+MongoCliente.connect(url, {useNewUrlParser: true, userUnifiedTopology: true }, function(err, client) {
+    assert.equal(null, err);
+    console.log("Aluno: Taina Braga Alecrim");
+    console.log("Servidor conectado");
+
+    const db = client.db(dbName);
+
+    findDocuments(db, function(){
+        client.close();
+    });
+});
